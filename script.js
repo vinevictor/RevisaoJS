4f87f7d234ff480512f6dad598f15e1bb05ad60b
Vinicius Victor <vine.victordev@gmail.com>
2023-02-02 20:00:16 -0300

    Tarefa 1.3

--- script.js
+++ script.js
@@ -9,6 +9,28 @@
 
 // 1.2
 
-var fraseUm = "caminho_5km&&corro_2Km, passo_o_dia_bem";
-var fraseDois = "levanto_cedo||antes_das_8h, saio_tranquilo";
-var fraseTres = "!termino_o_trabalho||!tenho_compromisso,saio_tarde";
+// var fraseUm = "caminho_5km&&corro_2Km, passo_o_dia_bem";
+// var fraseDois = "levanto_cedo||antes_das_8h, saio_tranquilo";
+// var fraseTres = "!termino_o_trabalho||!tenho_compromisso,saio_tarde";
+
+// 1.3
+
+var caminho = "5km";
+var corro = "2Km";
+
+if (caminho === "5km" && corro === "2Km") {
+  console.log("passo o di bem");
+}
+
+var levanto = "cedo";
+
+if (levanto === "cedo" || levanto === "antes das 8h") {
+  console.log("saio tranquilo");
+}
+
+var trabalhoTerminado = false;
+var compromisso = false;
+
+if (!trabalhoTerminado || !compromisso) {
+  console.log("saio tarde");
+}
