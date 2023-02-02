3d85a83cd67651d5d7c2b10afebd7fd530a4d227
Vinicius Victor <vine.victordev@gmail.com>
2023-02-02 19:36:26 -0300

    Tarefa 1.2

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var soma = 5 + 5;
-var numeroNegativo = -5 + -3;
-var div = 80 / 3;
-var rest = 80 % 3;
-soma + 5;
-soma - 3;
+// 1.1
+
+// var soma = 5 + 5;
+// var numeroNegativo = -5 + -3;
+// var div = 80 / 3;
+// var rest = 80 % 3;
+// soma + 5;
+// soma - 3;
+
+// 1.2
+
+var fraseUm = "caminho_5km&&corro_2Km, passo_o_dia_bem";
+var fraseDois = "levanto_cedo||antes_das_8h, saio_tranquilo";
+var fraseTres = "!termino_o_trabalho||!tenho_compromisso,saio_tarde";
