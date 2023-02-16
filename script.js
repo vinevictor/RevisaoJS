3b3c494492942e032052be8988a53272c7ade3cd
Vinicius Victor <vine.victordev@gmail.com>
2023-02-15 21:54:08 -0300

    Reforçando conceitos 1.5

--- script.js
+++ script.js
@@ -44,53 +44,70 @@
 
 // 1.4
 
-var lista = [12, 6, 9, 42, 11, 102, 44, 15];
-var soma = 0;
+// var lista = [12, 6, 9, 42, 11, 102, 44, 15];
+// var soma = 0;
 
-for (var i = 0; i < lista.length; i++) {
-  soma += lista[i];
+// for (var i = 0; i < lista.length; i++) {
+//   soma += lista[i];
+// }
+// console.log(soma);
+
+// soma = 0;
+
+// var contador = 0;
+// while (contador < lista.length) {
+//   soma += lista[contador];
+//   contador++;
+// }
+// console.log(soma);
+
+// var soma = 0;
+// var contador = 0;
+
+// do {
+//   soma += lista[contador];
+//   contador++;
+// } while (contador < lista.length);
+
+// console.log(soma);
+// var teste = true;
+// for (var i = 0; i < lista.length; i++) {
+//   for (var x = 2; x < lista[i]; x++) {
+//     if (lista[i] % x == 0 || lista[i] % (x + 1) == 0) {
+//       break;
+//     } else {
+//       teste = false;
+//     }
+//   }
+//   console.log(lista[i]);
+//   if (!teste) {
+//     break;
+//   }
+// }
+
+// contador = 0;
+// while (contador < lista.length) {
+//   if (lista[contador] >= 40 && lista[contador] <= 50) {
+//     contador++;
+//     continue;
+//   }
+//   console.log(lista[contador]);
+//   contador++;
+// }
+
+// 1.5
+var username;
+var password;
+
+function getUserNamePassword() {
+  username = prompt("Coloque o nome de usuario que deseja:");
+  password = prompt("Coloque a senha desejada:");
 }
-console.log(soma);
 
-soma = 0;
-
-var contador = 0;
-while (contador < lista.length) {
-  soma += lista[contador];
-  contador++;
-}
-console.log(soma);
-
-var soma = 0;
-var contador = 0;
-
-do {
-  soma += lista[contador];
-  contador++;
-} while (contador < lista.length);
-
-console.log(soma);
-var teste = true;
-for (var i = 0; i < lista.length; i++) {
-  for (var x = 2; x < lista[i]; x++) {
-    if (lista[i] % x == 0 || lista[i] % (x + 1) == 0) {
-      break;
-    } else {
-      teste = false;
-    }
-  }
-  console.log(lista[i]);
-  if (!teste) {
-    break;
-  }
-}
-
-contador = 0;
-while (contador < lista.length) {
-  if (lista[contador] >= 40 && lista[contador] <= 50) {
-    contador++;
-    continue;
-  }
-  console.log(lista[contador]);
-  contador++;
+function login(usernameLogin, passwordLogin) {
+  usernameLogin = prompt("Nome de usuario");
+  passwordLogin = prompt("Senha");
+  username === usernameLogin && password === passwordLogin
+    ? alert("Logado com sucesso...")
+    : alert("Nome de usuario/Senha errado...");
 }
